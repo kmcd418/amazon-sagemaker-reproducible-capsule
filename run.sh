af1b77f4bb02b81f3794e8d0c1a9f3a772a8ef4b
EC2 Default User <ec2-user@ip-172-16-15-58.eu-west-1.compute.internal>
2023-05-12 12:55:30 +0000

    add shel script

--- run.sh
+++ run.sh
@@ -1,21 +1,25 @@
 #!/usr/bin/env bash
 
 ##################
-# Save Capsule version to GitHub repo (commit code)
+# GET CAPSULE NAME AND GENERATE CAPSULE VERSION
 ##################
+# Read the YAML file
+yaml=$(cat config.yaml)
 
+CAPSULE_NAME=$(echo "$yaml" | sed -n 's/^ *capsule: *//p')
+CAPSULE_VERSION=$(uuidgen | cut -c-8)
 
 ##################
-# build Docker image and push it to ECR to be ready for use by SageMaker.
+# SAVE CAPSULE VERSION TO GITHUB REPO
 ##################
-image=$1
+echo "SAVING CAPSULE INTO GITHUB"
+# git add .
+# git commit -m CAPSULE_VERSION
+# git push
 
-if [[ "$image" == "" ]]
-then
-    echo "Usage: sh $0 <image-name>"
-    exit 1
-fi
-
+##################
+# BUILD AND PUSH CONTAINER TO ECR
+##################
 # Get the account number associated with the current IAM credentials
 account=$(aws sts get-caller-identity --query Account --output text)
 if [[ $? -ne 0 ]]
@@ -25,24 +29,28 @@ fi
 
 # Get the region defined in the current configuration (default to us-west-2 if none defined)
 region=$(aws configure get region)
-fullname="${account}.dkr.ecr.${region}.amazonaws.com/${image}:latest"
+fullname="${account}.dkr.ecr.${region}.amazonaws.com/${CAPSULE_NAME}:${CAPSULE_VERSION}"
 
 # If the repository doesn't exist in ECR, create it.
-aws ecr describe-repositories --repository-names "${image}" > /dev/null 2>&1
+aws ecr describe-repositories --repository-names "${CAPSULE_NAME}" > /dev/null 2>&1
 if [[ $? -ne 0 ]]
 then
-    aws ecr create-repository --repository-name "${image}" > /dev/null
+    aws ecr create-repository --repository-name "${CAPSULE_NAME}" > /dev/null
 fi
 
 # Get the login command from ECR and execute it directly
 $(aws ecr get-login --region ${region} --no-include-email)
 
 # Build the docker image locally with the image name and then push it to ECR
-# with the full name.
+echo "BUILDING CONTAINER IMAGE"
+docker build --no-cache -t ${CAPSULE_NAME} -f environment/Dockerfile .
+docker tag ${CAPSULE_NAME} ${fullname}
 
-echo "Building image with name ${image}"
-docker build --no-cache -t ${image} -f Dockerfile .
-docker tag ${image} ${fullname}
-
-echo "Pushing image to ECR ${fullname}"
+echo "PUSHING IMAGE TO ECR"
 docker push ${fullname}
+
+
+##################
+# LAUNCH JOB IN SAGEMAKER
+##################
+echo "LAUNCHING JOB RUN IN SAGEMAKER"
